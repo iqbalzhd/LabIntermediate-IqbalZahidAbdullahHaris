3f63bc2ff0e8a64fbdeb35a014aa01b312f00a5a
iqbalzhd <157446486+iqbalzhd@users.noreply.github.com>
2024-03-13 11:13:50 +0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,5 +1,5 @@
-urutan=6
-listTebak=[]
+urutan = 6
+listTebak = []
 kata = 'Software'
 kata = kata.lower()
 
@@ -7,27 +7,35 @@ while urutan > 0:
 
   tebak = input('Tebak huruf :')
   tebak = tebak.lower()
+
+
+  if tebak in listTebak:
+    print('huruf', tebak, 'sudah ditebak')
+
+  elif tebak in kata:
+
+    print('huruf', tebak, 'ada pada kata')
+
+  else:
+    urutan -= 1
+    print('huruf', tebak, 'tidak ada pada kata')
+
   listTebak.append(tebak)
 
-  kosong =''
+  kosong = ''
   for i in kata:
     if i in listTebak:
-      kosong+=i
+      kosong += i
     else:
-      kosong+='_'
-  if tebak in kata:
-    print('huruf',tebak,'ada pada kata')
-  else:
-    print('huruf',tebak,'tidak ada pada kata')
+      kosong += '_'
+
   print(kosong)
-  urutan -=1
+
   print('Kamu masih ada', urutan, 'kali lagi')
   if kosong == kata and urutan > 0:
     print('Selamat, kamu menang')
     break
   elif urutan <= 0:
-    print('Kamu kalah karena kehabisan waktu, kata yang benar adalah',kata.upper())
+    print('Kamu kalah karena kehabisan waktu, kata yang benar adalah',
+          kata.upper())
     break
-
-
-
