8303fa52e76ae2802673e8ac2b110051ac8c90f4
iqbalzhd <157446486+iqbalzhd@users.noreply.github.com>
2024-04-17 04:44:02 +0000

    17 April 2024

--- ice-cream.py
+++ ice-cream.py
@@ -15,8 +15,12 @@ while file != "":
   toko_3.append(hasilSplit[3])
   
   # buat dictionary
-  salesData[hasilSplit[0]] = [float(hasilSplit[1]), float(hasilSplit[2]), float(hasilSplit[2])]
+  salesData[hasilSplit[0]] = [float(hasilSplit[1]), float(hasilSplit[2]), float(hasilSplit[3])]
   file = iceCream.readline()
+
+print("%-12s %10s %10s %10s %10s" % ('Nama', 'Toko 1', 'Toko 2', 'Toko 3', 'Total'))
+for i in salesData:
+  print("%-12s %10.2f %10.2f %10.2f %10.2f" % (i, salesData[i][0], salesData[i][1],salesData[i][2], sum(salesData[i])))
 
 
   
